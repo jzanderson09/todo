001e98067474ab142b8973082c0fd12a23d30b77
Jac Anderson <jzanderson09@gmail.com>
2020-06-14 08:14:01 -0700

    Updates

---
+++ precache-manifest.aefed9c95155f8052d62d1fe8ef4c449.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990c08d18ec1e487b4d170b8b70e3be1",
+    "url": "/jzanderson09/todo/index.html"
+  },
+  {
+    "revision": "d4f2ebf07f3cb6a57625",
+    "url": "/jzanderson09/todo/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "3c92e8a0e708d5bc1a0d",
+    "url": "/jzanderson09/todo/static/css/main.051f2a72.chunk.css"
+  },
+  {
+    "revision": "d4f2ebf07f3cb6a57625",
+    "url": "/jzanderson09/todo/static/js/2.d57484f6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/jzanderson09/todo/static/js/2.d57484f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c92e8a0e708d5bc1a0d",
+    "url": "/jzanderson09/todo/static/js/main.c9cb7040.chunk.js"
+  },
+  {
+    "revision": "eb73c88e3cc9e9651d14",
+    "url": "/jzanderson09/todo/static/js/runtime-main.6c85f2da.js"
+  },
+  {
+    "revision": "3c14ea24afb3a8556ee27c74f533f58d",
+    "url": "/jzanderson09/todo/static/media/add-task.3c14ea24.mp3"
+  },
+  {
+    "revision": "aac5f019119212b2c55a82f962d59179",
+    "url": "/jzanderson09/todo/static/media/click-clear-selected.aac5f019.mp3"
+  },
+  {
+    "revision": "8e55e72a1eedf663f50fbff610f41505",
+    "url": "/jzanderson09/todo/static/media/click-completed.8e55e72a.mp3"
+  },
+  {
+    "revision": "852499a0b32352a5e0a0e55a0f2a6c58",
+    "url": "/jzanderson09/todo/static/media/click-done.852499a0.mp3"
+  },
+  {
+    "revision": "0d02e0e5da81e348b7feca681a25d83b",
+    "url": "/jzanderson09/todo/static/media/click-incomplete.0d02e0e5.mp3"
+  },
+  {
+    "revision": "502841189a3de09b9ddc8c571f93f77f",
+    "url": "/jzanderson09/todo/static/media/click-prompt.50284118.mp3"
+  },
+  {
+    "revision": "d8a660f253615de910bc83a079aeeaef",
+    "url": "/jzanderson09/todo/static/media/click-select-all.d8a660f2.mp3"
+  },
+  {
+    "revision": "d576f9be185595a9aff460a222fa03b7",
+    "url": "/jzanderson09/todo/static/media/corkboard-bg.d576f9be.jpg"
+  },
+  {
+    "revision": "eba816ab57fbea29f1a44ddc46ac7fef",
+    "url": "/jzanderson09/todo/static/media/error.eba816ab.mp3"
+  }
+]);
